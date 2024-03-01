3a7e7c6ba6acb89c1a33e6a9453e04789250f9fe
Johannes Krainer <j.krainer@posteo.de>
2024-03-01 18:41:41 +0100

    support 6 relais

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -14,12 +14,20 @@ type response struct {
 	State bool `json:"state"`
 }
 
-var mu sync.Mutex
-
 func main() {
 
+	var mu sync.Mutex
+
+	var useGPIOsForRelais [6]uint = [...]uint{
+		17,
+		22,
+		23,
+		24,
+		25,
+		27,
+	}
+
 	useDevice := "/dev/gpiochip0"
-	useOffset := 22
 
 	d := gpiod.NewDevice(useDevice)
 	err := d.Open()
@@ -30,52 +38,61 @@ func main() {
 	defer d.Close()
 	log.Println("successfully opened device")
 
-	err = d.AddLine(uint(useOffset), gpiod.LineDirectionOutput)
-	if err != nil {
-		log.Println(err)
-		return
+	for i, offset := range useGPIOsForRelais {
+
+		err = d.AddLine(offset, gpiod.LineDirectionOutput)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		log.Println("successfully added line", offset)
+
+		route := fmt.Sprintf("/relais%d/state", i)
+
+		http.HandleFunc(route, func(w http.ResponseWriter, r *http.Request) {
+			mu.Lock()
+			defer mu.Unlock()
+
+			currentValue, err := d.GetLineValue(offset)
+			if err != nil {
+				log.Println(err)
+			}
+			w.Header().Set("Content-Type", "application/json")
+			jenc := json.NewEncoder(w)
+			state := true
+			if currentValue == gpiod.LineValueInactive {
+				state = true
+			}
+			jenc.Encode(response{state})
+		})
+
+		route = fmt.Sprintf("/relais%d/on", i)
+
+		http.HandleFunc(route, func(w http.ResponseWriter, r *http.Request) {
+			mu.Lock()
+			defer mu.Unlock()
+
+			err := d.SetLineValue(offset, gpiod.LineValueActive)
+			if err != nil {
+				log.Println(err)
+			}
+			// TODO post current state
+		})
+
+		route = fmt.Sprintf("/relais%d/off", i)
+
+		http.HandleFunc(route, func(w http.ResponseWriter, r *http.Request) {
+			mu.Lock()
+			defer mu.Unlock()
+
+			err := d.SetLineValue(offset, gpiod.LineValueInactive)
+			if err != nil {
+				log.Println(err)
+			}
+			// TODO post current state
+		})
+
 	}
-	log.Println("successfully added line")
-
-	http.HandleFunc("/relais1/state", func(w http.ResponseWriter, r *http.Request) {
-		mu.Lock()
-		defer mu.Unlock()
-
-		currentValue, err := d.GetLineValue(uint(useOffset))
-		if err != nil {
-			log.Println(err)
-		}
-		w.Header().Set("Content-Type", "application/json")
-		jenc := json.NewEncoder(w)
-		state := true
-		if currentValue == gpiod.LineValueInactive {
-			state = true
-		}
-		jenc.Encode(response{state})
-		fmt.Fprintf(w, "Hello, %q")
-	})
-
-	http.HandleFunc("/relais1/on", func(w http.ResponseWriter, r *http.Request) {
-		mu.Lock()
-		defer mu.Unlock()
-
-		err := d.SetLineValue(uint(useOffset), gpiod.LineValueActive)
-		if err != nil {
-			log.Println(err)
-		}
-		// TODO post current state
-	})
-
-	http.HandleFunc("/relais1/off", func(w http.ResponseWriter, r *http.Request) {
-		mu.Lock()
-		defer mu.Unlock()
-
-		err := d.SetLineValue(uint(useOffset), gpiod.LineValueInactive)
-		if err != nil {
-			log.Println(err)
-		}
-		// TODO post current state
-	})
 
 	http.ListenAndServe(":8090", nil)
 }
