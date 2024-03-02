2291b17c402ea5c651ba926af57f797202b8f074
Johannes Krainer <j.krainer@posteo.de>
2024-03-02 17:35:39 +0100

    state as array

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -6,10 +6,18 @@ import (
 	"net/http"
 	"sync"
 
-	"github.com/shamaton/msgpack/v2"
-
 	"github.com/JoKr4/gpiod2go/pkg/gpiod"
+	"github.com/shamaton/msgpack/v2"
 )
+
+// type relaisState uint8
+
+// const (
+// 	unknown relaisState = iota
+// 	off     relaisState
+// 	on      relaisState
+// 	erro    relaisState
+// )
 
 func main() {
 
@@ -35,6 +43,37 @@ func main() {
 	defer d.Close()
 	log.Println("successfully opened device")
 
+	route := "/relais/states"
+
+	http.HandleFunc(route, func(w http.ResponseWriter, r *http.Request) {
+		mu.Lock()
+		defer mu.Unlock()
+
+		result := make([]bool, len(useGPIOsForRelais))
+		for i, offset := range useGPIOsForRelais {
+			result[i] = false // TODO unknown
+			currentValue, err := d.GetLineValue(offset)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			if currentValue == gpiod.LineValueActive {
+				result[i] = true
+			}
+			result[i] = currentValue
+		}
+		w.Header().Set("Content-Type", "application/msgpack")
+		d, err := msgpack.Marshal(state)
+		if err != nil {
+			log.Println(err)
+		}
+		_, err = w.Write(d)
+		if err != nil {
+			log.Println(err)
+		}
+		log.Println("responded state request of", r.RemoteAddr)
+	})
+
 	for i, offset := range useGPIOsForRelais {
 
 		err = d.AddLine(offset, gpiod.LineDirectionOutput)
@@ -43,32 +82,6 @@ func main() {
 			continue
 		}
 		log.Println("successfully added line", offset)
-
-		route := fmt.Sprintf("/relais%d/state", i)
-
-		http.HandleFunc(route, func(w http.ResponseWriter, r *http.Request) {
-			mu.Lock()
-			defer mu.Unlock()
-
-			currentValue, err := d.GetLineValue(offset)
-			if err != nil {
-				log.Println(err)
-			}
-			w.Header().Set("Content-Type", "application/msgpack")
-			state := true
-			if currentValue == gpiod.LineValueInactive {
-				state = false
-			}
-			d, err := msgpack.Marshal(state)
-			if err != nil {
-				log.Println(err)
-			}
-			_, err = w.Write(d)
-			if err != nil {
-				log.Println(err)
-			}
-			log.Println("responded state request of", r.RemoteAddr)
-		})
 
 		route = fmt.Sprintf("/relais%d/on", i)
 
